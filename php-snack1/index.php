64e29233c7963362b805baa7279d9c37d4b5aba4
Nicola <nicolusso@gmail.com>
2021-02-25 15:46:21 +0100

    1Snack completato

--- php-snack1/index.php
+++ php-snack1/index.php
@@ -5,31 +5,40 @@ Stampiamo a schermo tutte le partite -->
 <!-- Dichiarazioni Variabili -->
 
 <?php
-  $matches =[
-    'casa' => 'Olimpia Milano',
-    'ospite' => 'Cantù',
-    'punteggio' => '67-69',
-  ],
+$matches = [
   [
-    'casa' => 'Dinamo Sassari',
-    'ospite' => 'Umana Venezia',
-    'punteggio' => '104-87',
-  ],
-  [
-    'casa' => 'Reggio Emilia',
-    'ospite' => 'Trieste',
-    'punteggio' => '65-70',
-  ],
-  [
-    'casa' => 'Vanoli Cremona',
-    'ospite' => 'Prosciutto Pesaro',
-    'punteggio' => '78-83',
-  ],
-  [
-    'casa' => 'Varese',
-    'ospite' => 'Virtus Bologna',
-    'punteggio' => '70-93',
-  ],
+  'casa' => 'Olimpia Milano',
+  'ospite' => 'Cantù',
+  'punteggioCasa' => '65',
+  'punteggioOspite' => '88',
+ ],
+ [
+  'casa' => 'Dinamo Sassari',
+  'ospite' => 'Umana Venezia',
+  'punteggioCasa' => '105',
+  'punteggioOspite' => '77',
+ ],
+ [
+  'casa' => 'Reggio Emilia',
+  'ospite' => 'Trieste',
+  'punteggioCasa' => '54',
+  'punteggioOspite' => '63',
+ ],
+ [
+  'casa' => 'Vanoli Cremona',
+  'ospite' => 'Prosciutto Pesaro',
+  'punteggioCasa' => '69',
+  'punteggioOspite' => '71',
+ ],
+ [
+  'casa' => 'Varese',
+  'ospite' => 'Virtus Bologna',
+  'punteggioCasa' => '70',
+  'punteggioOspite' => '93',
+]
+];
+
+
  ?>
 
 <!DOCTYPE html>
@@ -39,6 +48,13 @@ Stampiamo a schermo tutte le partite -->
     <title>Basket Matches</title>
   </head>
   <body>
+    <?php for($i = 0 ; $i < count($matches); $i++) { ?>
+    <h3>MATCH</h3>
+    <h4><?php echo $matches[$i]['casa'] . ' vs ' . $matches[$i]['ospite'] .  ' | '  . $matches[$i]['punteggioCasa'] .' - ' . $matches[$i]['punteggioOspite']; ?></h4>
+
+​
+     <?php  }   ?>
+
 
   </body>
 </html>
