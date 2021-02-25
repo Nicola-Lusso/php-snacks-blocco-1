2ed6f1d23320eaa7668b268706002bdd40dcb34f
Nicola <nicolusso@gmail.com>
2021-02-25 14:59:05 +0100

    Parte Iniziale (Matches dichiarati)

---
+++ php-snack1/index.php
@@ -0,0 +1,44 @@
+<!-- Creiamo un array 'matches' contenente altri array che rappresentano delle partite di basket;
+Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+Stampiamo a schermo tutte le partite -->
+
+<!-- Dichiarazioni Variabili -->
+
+<?php
+  $matches =[
+    'casa' => 'Olimpia Milano',
+    'ospite' => 'Cantù',
+    'punteggio' => '67-69',
+  ],
+  [
+    'casa' => 'Dinamo Sassari',
+    'ospite' => 'Umana Venezia',
+    'punteggio' => '104-87',
+  ],
+  [
+    'casa' => 'Reggio Emilia',
+    'ospite' => 'Trieste',
+    'punteggio' => '65-70',
+  ],
+  [
+    'casa' => 'Vanoli Cremona',
+    'ospite' => 'Prosciutto Pesaro',
+    'punteggio' => '78-83',
+  ],
+  [
+    'casa' => 'Varese',
+    'ospite' => 'Virtus Bologna',
+    'punteggio' => '70-93',
+  ],
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Basket Matches</title>
+  </head>
+  <body>
+
+  </body>
+</html>
