b65d6a690c00b60eb7f85369f7b6a43ed7f2aa46
Nicola <nicolusso@gmail.com>
2021-02-25 15:51:23 +0100

    Dichiarazione Variabili Snack2

---
+++ php-Snack2/index.php
@@ -0,0 +1,24 @@
+<!-- Passare come parametri GET name, mail e age e verificare
+1. name sia più lungo di 3 caratteri,
+2. mail contenga un punto e una chiocciola
+3. age sia un numero. -->
+
+<!-- DICHIARAZIONE VARIABILI -->
+<?php
+
+$name = $_GET['name'];
+$age = $_GET['age'];
+$mail = $_GET['mail'];
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
